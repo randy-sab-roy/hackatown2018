1bacd415ef9e761085c83efaa6b20e21d454ff14
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-01-20 16:01:06 -0500

    pour Simon

--- main.cpp
+++ main.cpp
@@ -9,6 +9,112 @@
 
 using namespace cv;
 
+static char INPUT_DATA_FILE[] = "input.data";
+static char INPUT_CFG_FILE[] = "input.cfg";
+static char INPUT_WEIGHTS_FILE[] = "input.weights";
+static char INPUT_AV_FILE[] = "input.mp4";
+
 int main()
 {
+    ArapahoV2 *darknet = new ArapahoV2();
+    ArapahoV2Params params;
+
+    params.datacfg = INPUT_DATA_FILE;
+    params.cfgfile = INPUT_CFG_FILE;
+    params.weightfile = INPUT_WEIGHTS_FILE;
+    params.nms = 0.4;
+    params.maxClasses = 4;
+
+    int expectedWidth = 0;
+    int expectedHeight = 0;
+
+    bool isSuccessful = darknet.Setup(params, expectedWidth, expectedHeight);
+    if (!isSuccessful)
+    {
+        return -1;
+    }
+
+    ArapahoV2ImageBuff imageBuffer;
+    Mat image;
+
+    namedWindow("Guardius", CV_WINDOW_AUTOSIZE);
+
+    while (1)
+    {
+        bool success = cap.read(image);
+
+        if (!success || image.empty())
+        {
+            if (p)
+                delete p;
+            p = nullptr;
+            waitKey();
+            return -1;
+        }
+
+        imageWidthPixels = image.size().width;
+        imageHeightPixels = image.size().height;
+
+        // Remember the time
+        auto detectionStartTime = std::chrono::system_clock::now();
+
+        // Process the image
+        imageBuffer.bgr = image.data;
+        imageBuffer.w = imageWidthPixels;
+        imageBuffer.h = imageHeightPixels;
+        imageBuffer.channels = 3;
+
+        int numObjects = 0;
+
+        // Detect the objects in the image
+        p->Detect(image, 0.24, 0.5, numObjects);
+
+        std::chrono::duration<double> detectionTime = (std::chrono::system_clock::now() - detectionStartTime);
+
+        if (numObjects > 0)
+        {
+            boxes = new box[numObjects];
+            labels = new std::string[numObjects];
+
+            // Get boxes and labels
+            p->GetBoxes(boxes, labels, numObjects);
+
+            int leftTopX = 0, leftTopY = 0, rightBotX = 0, rightBotY = 0;
+            for (int objId = 0; objId < numObjects; objId++)
+            {
+                leftTopX = 1 + imageWidthPixels * (boxes[objId].x - boxes[objId].w / 2);
+                leftTopY = 1 + imageHeightPixels * (boxes[objId].y - boxes[objId].h / 2);
+                rightBotX = 1 + imageWidthPixels * (boxes[objId].x + boxes[objId].w / 2);
+                rightBotY = 1 + imageHeightPixels * (boxes[objId].y + boxes[objId].h / 2);
+
+                // Show image and overlay using OpenCV
+                rectangle(image,
+                          cvPoint(leftTopX, leftTopY),
+                          cvPoint(rightBotX, rightBotY),
+                          CV_RGB(255, 0, 0), 1, 8, 0);
+                // Show labels
+                if (labels[objId].c_str())
+                {
+                    DPRINTF("Label:%s\n\n", labels[objId].c_str());
+                    putText(image, labels[objId].c_str(), cvPoint(leftTopX, leftTopY),
+                            FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(200, 200, 250), 1, CV_AA);
+                }
+            }
+
+            if (boxes)
+            {
+                delete[] boxes;
+                boxes = NULL;
+            }
+            if (labels)
+            {
+                delete[] labels;
+                labels = NULL;
+            }
+        }
+        imshow("Arapaho", image);
+        waitKey((1000 / TARGET_SHOW_FPS));
+    }
+
+    return 0;
 }
