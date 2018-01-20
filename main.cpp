e7c5a27284aab9f94919323cbfe5b3f11d325a6a
Randy Sab-Roy <randy.sab.roy@gmail.com>
2018-01-20 13:14:47 -0500

    puish

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "arapaho.hpp"
+#include <string>
+#include "opencv2/core/core.hpp"
+#include <opencv2/imgproc.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <chrono>
+
+using namespace cv;
+
+int main()
+{
+}
